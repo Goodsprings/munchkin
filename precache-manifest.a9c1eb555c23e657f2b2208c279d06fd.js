5a718465d9571242dcc60411b77060275daee9c8
seva <seva.voloshin@gmail.com>
2018-10-06 02:05:14 +0300

    Updates

---
+++ precache-manifest.a9c1eb555c23e657f2b2208c279d06fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70765e3c1db9b2387474",
+    "url": "/munchkin/static/js/runtime~main.70765e3c.js"
+  },
+  {
+    "revision": "106ecb7eb571882363d6",
+    "url": "/munchkin/static/js/main.106ecb7e.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/munchkin/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "106ecb7eb571882363d6",
+    "url": "/munchkin/static/css/main.15528311.chunk.css"
+  },
+  {
+    "revision": "98351923d56e8bc5ed5a3b01dcd3ed26",
+    "url": "/munchkin/index.html"
+  }
+];
