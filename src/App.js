ec1b3c1c81c5e28b1a2695f77e72c1f63a2ea018
seva <seva.voloshin@gmail.com>
2018-10-06 01:55:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: 1,
+      second: 1,
+      third: 1,
+      fourth: 1
+    };
+  }
+
+  upLvl = (player) => {
+    switch(player) {
+      case 1:
+        if(this.state.first < 10)
+          this.setState({ first: ++this.state.first});
+        break;
+      case 2:
+        if(this.state.second < 10)
+          this.setState({ second: ++this.state.second});
+        break;
+      case 3:
+        if(this.state.third < 10)
+          this.setState({ third: ++this.state.third});
+        break;
+      case 4:
+        if(this.state.fourth < 10)
+          this.setState({ fourth: ++this.state.fourth});
+        break;
+      default:
+        console.log("default");
+    }
+  }
+
+  downLvl = (player) => {
+    switch(player) {
+      case 1:
+        if(this.state.first > 1)
+          this.setState({ first: --this.state.first});
+        break;
+      case 2:
+        if(this.state.second > 1)
+          this.setState({ second: --this.state.second});
+        break;
+      case 3:
+        if(this.state.third > 1)
+          this.setState({ third: --this.state.third});
+        break;
+      case 4:
+        if(this.state.fourth > 1)
+          this.setState({ fourth: --this.state.fourth});
+        break;
+      default:
+        console.log("default");
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <table>
+          <tbody>
+            <tr>
+              <td rowSpan="2">First:</td>
+              <td rowSpan="2">{ this.state.first }</td>
+              <td><a href={null} onClick={ () => { this.upLvl(1) } }>▲</a></td>
+            </tr>
+            <tr>
+              <td><a href={null} onClick={ () => { this.downLvl(1) } }>▼</a></td>
+            </tr>
+            <tr>
+              <td rowSpan="2" style={{paddingRight:15}}>Second:</td>
+              <td rowSpan="2">{ this.state.second }</td>
+              <td><a href={null} onClick={ () => { this.upLvl(2) } }>▲</a></td>
+            </tr>
+            <tr>
+              <td><a href={null} onClick={ () => { this.downLvl(2) } }>▼</a></td>
+            </tr>
+            <tr>
+              <td rowSpan="2">Third:</td>
+              <td rowSpan="2">{ this.state.third }</td>
+              <td><a href={null} onClick={ () => { this.upLvl(3) } }>▲</a></td>
+            </tr>
+            <tr>
+              <td><a href={null} onClick={ () => { this.downLvl(3) } }>▼</a></td>
+            </tr>
+            <tr>
+              <td rowSpan="2">Fourth:</td>
+              <td rowSpan="2">{ this.state.fourth }</td>
+              <td><a href={null} onClick={ () => { this.upLvl(4) } }>▲</a></td>
+            </tr>
+            <tr>
+              <td><a href={null} onClick={ () => { this.downLvl(4) } }>▼</a></td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <a href={null} onClick={
+            () => {
+              this.setState({
+                first: 1,
+                second: 1,
+                third: 1,
+                fourth: 1
+              });
+            }
+          }>
+          New game
+        </a>
       </div>
     );
   }
